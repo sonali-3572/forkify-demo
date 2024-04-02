d60371c9e1f7009f8d2f9ae1131c0949426567c2
sonali-3572 <sonalichaurasiya001@gmail.com>
2024-04-02 17:15:28 +0530

    'Initial'

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5472fb0f-47e0-45b8-ad70-a1adb8334704';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/controller.js
@@ -0,0 +1,157 @@
+// npm fractional is external library to convert 1.5 like no into 1 and 1/2
+// npm install fractional
+
+import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import Bookmarksview from './views/bookmarksView.js';
+import paginationView from './views/paginationView.js';
+import addRecipeView from './views/addRecipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import recipeView from './views/recipeView.js';
+import bookmarksView from './views/bookmarksView.js';
+
+// 1. (npm init) it will create package.json
+// 2. (npm i parcel -D ) install latest version of parcel available on npm
+// 3. now start parcel (npm run start)
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    // console.log(id); //id of recipe
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
+    // 1. loading recipe
+    await model.loadRecipe(id);
+    // const recipe = model.state.recipe;
+
+    // 2. rendering recipe
+    recipeView.render(model.state.recipe);
+    // const recipeView = new recipeView(model.state.recipe);
+
+    //updating bookmark view
+    bookmarksView.update(model.state.bookmarks);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // 1. get search query
+    const query = searchView.getQuery();
+    if (!query && query === '') return;
+
+    // 2. load search results
+    await model.loadSearchResults(query);
+
+    // 3. render results
+    // console.log(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(1));
+
+    // 4. render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+const controlPagination = function (goToPage) {
+  // 3. render new results
+  // console.log(model.state.search.results);
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 4. render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  // Update the recipe servings(in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  // Add /remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else if (model.state.recipe.bookmarked)
+    model.deletBookmark(model.state.recipe.id);
+
+  // update the recipe view
+  recipeView.update(model.state.recipe);
+
+  //render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  // upload new recipe data
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // render recipe
+    recipeView.render(model.state.recipe);
+
+    // success message
+    addRecipeView.renderMessage();
+
+    // add bookmark
+    bookmarksView.render(model.state.bookmarks);
+
+    // change id in the url
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+    // window.history.back();
+
+    // close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    addRecipeView.renderError(err.message);
+  }
+};
+
+const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+  // controlServings();
+};
+init();
+
+// 1. (git init)
+// 2. local git installation connection with your github account
+//
+// 3. (git statue )  get status of all the files
+// 4. (git add -A)   to track all the files
+// 5. commit files (git commit -m 'Initial commit')
